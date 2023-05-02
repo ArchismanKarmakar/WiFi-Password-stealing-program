6943dd922f8578e483bff6ad3975d9a503dc8c74
Archisman Karmakar <92569441+ArchismanKarmakar@users.noreply.github.com>
2023-05-02 23:42:34 +0530

    decrypting data

--- Encrypt-Decrypt.py
+++ Encrypt-Decrypt.py
@@ -4,3 +4,13 @@ key = Fernet.generate_key()
 file = open("encryption_key.txt", 'wb')
 file.write(key)
 file.close()
+
+WiFi_pass = "information-wifi.txt"
+
+encrypted_file = [WiFi_pass]
+c = 0
+
+
+for decrypting_files in encrypted_file:
+    with open(encrypted_file[c], 'rb') as f:
+        data = f.read()
